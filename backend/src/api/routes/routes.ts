4d3119f82c53f6f1c43d120e783eea43f227907c
marcos <marc0sdurval20@gmail.com>
2022-05-19 10:30:30 -0300

    feat: connecta a rota getAll com o app

--- backend/src/api/routes/routes.ts
+++ backend/src/api/routes/routes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import * as controller from '../../app/controller/task.controller';
 
 const routerTask = Router();
 
-routerTask.get('/');
+routerTask.get('/', async (req:Request, res:Response) => {
+  const allTasks = await controller.findAll();
+  return res.status(200).json(allTasks);
+});
 
 routerTask.get('/:id');
 
