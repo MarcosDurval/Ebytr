35e5718af8fe897dfd94a5217047671897ec274e
marcos <marc0sdurval20@gmail.com>
2022-05-19 14:39:54 -0300

    feat: adiciona o middleware de erro

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes/routes';
+import erros from './middleware/erros';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/task', routes);
+
+app.use(erros);
 
 export default app;
