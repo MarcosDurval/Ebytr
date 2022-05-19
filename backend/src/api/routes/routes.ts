a7a55e25ac80322dd122ba09cc1d9da24d840fa9
marcos <marc0sdurval20@gmail.com>
2022-05-19 13:36:19 -0300

    feat: conecta a rota task/:id com o controller

--- backend/src/api/routes/routes.ts
+++ backend/src/api/routes/routes.ts
@@ -8,7 +8,11 @@ routerTask.get('/', async (req:Request, res:Response) => {
   return res.status(200).json(allTasks);
 });
 
-routerTask.get('/:id');
+routerTask.get('/:id', async (req:Request, res:Response) => {
+  const { id } = req.params;
+  const task = await controller.findOne(id);
+  return res.status(200).json(task);
+});
 
 routerTask.post('/');
 
