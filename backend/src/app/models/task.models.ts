ac40b0c4a0a3d0d99de7d896d69a986e56545592
marcos <marc0sdurval20@gmail.com>
2022-05-19 11:39:33 -0300

    refactor: atualiza nome da interface

--- backend/src/app/models/task.models.ts
+++ backend/src/app/models/task.models.ts
@@ -4,4 +4,4 @@ export const findAll = async () => taskModel.find();
 
 export const findOne = async (id:ItaskId['id']) => taskModel.findOne({ _id: id });
 
-export const create = async (body:Itask) => taskModel.create(body);
+export const create = async (body:createTask) => taskModel.create(body);
--- backend/src/app/services/task.services.ts
+++ backend/src/app/services/task.services.ts
@@ -4,4 +4,4 @@ export const findAll = async () => models.findAll();
 
 export const findOne = async (id:ItaskId['id']) => models.findOne(id);
 
-export const create = async (body:Itask) => models.create(body);
+export const create = async (body:createTask) => models.create(body);
