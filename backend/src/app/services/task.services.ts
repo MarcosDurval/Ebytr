63725a81173a9e44630be2809266acfe868f041f
marcos <marc0sdurval20@gmail.com>
2022-05-19 08:51:58 -0300

    feat: adiciona ao service as funções findAll, findOne e create

---
+++ backend/src/app/services/task.services.ts
@@ -0,0 +1,7 @@
+import * as models from '../models/task.models';
+
+export const findAll = async () => models.findAll();
+
+export const findOne = async (id:ItaskId['id']) => models.findOne(id);
+
+export const create = async (body:Itask) => models.create(body);
