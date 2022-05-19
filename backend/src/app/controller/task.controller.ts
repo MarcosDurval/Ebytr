cf523883d9690444b3ea9c6d6a17e31e6f24e002
marcos <marc0sdurval20@gmail.com>
2022-05-19 13:35:13 -0300

    feat: adiciona validação nos dados recebidos no create

--- backend/src/app/controller/task.controller.ts
+++ backend/src/app/controller/task.controller.ts
@@ -1,7 +1,11 @@
 import * as service from '../services/task.services';
+import isValidTask from './validInput/validTask';
 
 export const findAll = async () => service.findAll();
 
 export const findOne = async (id:ItaskId['id']) => service.findOne(id);
 
-export const create = async (body:IcreateTask) => service.create(body);
+export const create = async (body:IcreateTask) => {
+  await isValidTask(body);
+  return service.create(body);
+};
