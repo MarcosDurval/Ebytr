2802d50611877a0a7bd763213660482a61d88c58
marcos <marc0sdurval20@gmail.com>
2022-05-19 13:37:18 -0300

    feat: verifica se a task existe

--- backend/src/app/services/task.services.ts
+++ backend/src/app/services/task.services.ts
@@ -2,6 +2,12 @@ import * as models from '../models/task.models';
 
 export const findAll = async () => models.findAll();
 
-export const findOne = async (id:ItaskId['id']) => models.findOne(id);
+export const findOne = async (id:ItaskId['id']) => {
+  const task = await models.findOne(id);
+  if (!task) {
+    throw new Error('404/task not found');
+  }
+  return task;
+};
 
 export const create = async (body:IcreateTask) => models.create(body);
