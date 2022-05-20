05f901bc73bf7687cf026735fca1e986cf797804
marcos <marc0sdurval20@gmail.com>
2022-05-19 23:31:07 -0300

    refactor:  atualiza schema

--- backend/src/@types/task/interfacetask.ts
+++ backend/src/@types/task/interfacetask.ts
@@ -1,7 +1,5 @@
 interface IcreateTask {
-  title:string
   task:string,
-  author:string
 }
 
 interface ItaskId extends IcreateTask {
--- backend/src/app/controller/validInput/validTask.ts
+++ backend/src/app/controller/validInput/validTask.ts
@@ -2,9 +2,7 @@ import { z } from 'zod';
 
 const isValidTask = async (body:IcreateTask) => {
   const schemaTask = z.object({
-    title: z.string().min(3),
     task: z.string().min(5),
-    author: z.string().min(3),
   });
   await schemaTask.parseAsync(body);
 };
