817ccb15b3fa49dd9c83926f7e75a760241e2b5c
marcos <marc0sdurval20@gmail.com>
2022-05-20 11:01:24 -0300

    tests: adiciona teste unitarios no service e controller

--- backend/test/unit/controller.test.ts
+++ backend/test/unit/controller.test.ts
@@ -1,15 +1,54 @@
 import sinon from 'sinon'
 import { expect } from 'chai'
-import * as tasckService from '../../src/app/services/task.services'
+import * as taskService from '../../src/app/services/task.services'
 import * as taskController from '../../src/app/controller/task.controller'
 
 describe('Funções do controller',() => {
   afterEach(sinon.restore)
   describe('FindAll',() => {
-    sinon.stub(tasckService,'findAll').resolves([])
     it('testa se o controller tem retorno',async () => {
+      sinon.stub(taskService,'findAll').resolves([])
       const result = await taskController.findAll()
       expect(result).to.be.an('array')
+    })
+  })
+  describe('FindOne',() => {
+    const id = '12345'
+    it('testa se o controller tem retorno',async () => {
+      sinon.stub(taskService,'findOne').resolves({} as unknown as ItaskId)
+      const result = await taskController.findOne(id)
+
+      expect(result).to.be.an('object')
+    })
+  })
+  describe('Create',() => {
+    let task = 'batman lavar louça'
+    it('testa se o controller tem retorno',async () => {
+      sinon.stub(taskService,'create').resolves({} as ItaskId)
+      const result = await taskController.create({task})
+      expect(result).to.be.an('object')
+    })
+  })
+  describe('update',() => {
+    const obj ={
+      id:'12345',
+     task:'batman lavar louça',
+      date: new Date(),
+      status:'pendente'
+    } 
+    const id = '12345'
+    it('testa se o controller tem retorno',async () => {
+      sinon.stub(taskService,'update').resolves()
+      const result = await taskController.update(id,obj)
+      expect(result).to.be
+    })
+  })
+  describe('Delete',() => {
+    const id = '12345'
+    it('testa se o controller tem retorno',async () => {
+      sinon.stub(taskService,'destroy').resolves()
+      const result = await taskController.destroy(id)
+      expect(result).to.be
     })
   })
 })
