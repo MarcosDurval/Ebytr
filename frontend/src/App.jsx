bbb3b1cb00796fad697f5cc46707228c0cf16176
marcos <marc0sdurval20@gmail.com>
2022-05-20 07:50:46 -0300

    feat: adiciona a pagina details as rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import List from './pages/ListTask';
 import NewTask from './pages/newTask';
+import Details from './pages/details';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<List />} />
       <Route path="/create" element={<NewTask />} />
+      <Route path=":id" element={<Details />} />
     </Routes>
   );
 }
